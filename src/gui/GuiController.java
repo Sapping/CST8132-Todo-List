72dae95c6b03a79b206c8a032df97dd8346f8e62
Sapping <lucas.melin@gmail.com>
2016-05-18 14:54:03 -0400

    Added priorityChoiceBox

--- src/gui/GuiController.java
+++ src/gui/GuiController.java
@@ -3,9 +3,12 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -15,16 +18,29 @@ public class GuiController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		assert priorityChoiceBox != null : "fx:id=\"priorityChoiceBox\" was not injected: check your FXML file 'TodoGui.fxml'.";
+		priorityChoiceBox.getItems().addAll("High","Medium","Low"); // Add all possible elements to priorityChoiceBox
+		priorityChoiceBox.getSelectionModel().select(0); // Set default selection to item[0]
 	}
 	
 	@FXML
-	Button addButton;
+	private Button addButton;
 	@FXML
-	TextField descriptionTextField;
+	private TextField descriptionTextField;
 	@FXML
+	private 
 	ListView<Task> listView;
+	@FXML //fx:id="priorityChoiceBox"
+	private ChoiceBox<String> priorityChoiceBox;
+	
+
+	
+	@FXML
+	private void addTask(Event e){
+		
+	}
+
+
 	
 
 }
